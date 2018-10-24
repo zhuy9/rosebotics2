8c4006c810bef61bfaccdd7816dfa43574eb41b7
DavidMutchler <mutchler@rose-hulman.edu>
2018-10-24 08:01:48 -0400

    Ready enough for students, I think.

--- src/m1.py
+++ src/m1.py
@@ -1,7 +1,5 @@
 """
-  Capstone Project.  Code for testing basics.
-  Author:  David Mutchler, based on work by Dave Fisher and others.
-  READ and RUN this module but ** DO NOT MODIFY IT. **
+  Capstone Project.  Code written by PUT_YOUR_NAME_HERE.
   Fall term, 2018-2019.
 """
 
@@ -10,93 +8,7 @@ import time
 
 
 def main():
-    """ Runs tests. """
-    run_tests()
-
-
-def run_tests():
-    """ Runs various tests. """
-    # run_test_drive_system()
-    # run_test_touch_sensor()
-    run_test_color_sensor()
-
-
-def run_test_drive_system():
-    """ Tests the  drive_system  of the Snatch3rRobot. """
-    robot = rb.Snatch3rRobot()
-
-    print()
-    print("Testing the  drive_system  of the robot.")
-    print("Move at (20, 50) - that is, veer left slowly")
-    robot.drive_system.start_moving(20, 50)
-    time.sleep(2)
-    robot.drive_system.stop_moving()
-
-    print("Left/right wheel positions:",
-          robot.drive_system.left_wheel.get_degrees_spun(),
-          robot.drive_system.right_wheel.get_degrees_spun())
-
-    time.sleep(1)
-    print()
-    print("Spin clockwise at half speed for 2.5 seconds")
-    robot.drive_system.move_for_seconds(2.5, 50, -50)
-
-    print("Left/right wheel positions:",
-          robot.drive_system.left_wheel.get_degrees_spun(),
-          robot.drive_system.right_wheel.get_degrees_spun())
-
-    robot.drive_system.left_wheel.reset_degrees_spun()
-    robot.drive_system.right_wheel.reset_degrees_spun(2000)
-
-    time.sleep(1)
-    print()
-    print("Move forward at full speed for 1.5 seconds, coast to stop")
-    robot.drive_system.start_moving()
-    time.sleep(1.5)
-    robot.drive_system.stop_moving(rb.StopAction.COAST)
-
-    print("Left/right wheel positions:",
-          robot.drive_system.left_wheel.get_degrees_spun(),
-          robot.drive_system.right_wheel.get_degrees_spun())
-
-
-def run_test_touch_sensor():
-    """ Tests the  touch_sensor  of the Snatch3rRobot. """
-    robot = rb.Snatch3rRobot()
-
-    print()
-    print("Testing the  touch_sensor  of the robot.")
-    print("Repeatedly press and release the touch sensor.")
-    print("Press Control-C when you are ready to stop testing.")
-    time.sleep(1)
-    count = 1
-    while True:
-        print("{:4}.".format(count),
-              "Touch sensor value is: ", robot.touch_sensor.get_value())
-        time.sleep(0.5)
-        count = count + 1
-
-
-def run_test_color_sensor():
-    """ Tests the  color_sensor  of the Snatch3rRobot. """
-    robot = rb.Snatch3rRobot()
-
-    print()
-    print("Testing the  color_sensor  of the robot.")
-    print("Repeatedly move the robot to different surfaces.")
-    print("Press Control-C when you are ready to stop testing.")
-    time.sleep(1)
-    count = 1
-    while True:
-        print("{:4}.".format(count),
-              "Color sensor value/color/intensity is: ",
-              "{:3} {:3} {:3}".format(robot.color_sensor.get_value()[0],
-                                      robot.color_sensor.get_value()[1],
-                                      robot.color_sensor.get_value()[2]),
-              "{:4}".format(robot.color_sensor.get_color()),
-              "{:4}".format(robot.color_sensor.get_reflected_intensity()))
-        time.sleep(0.5)
-        count = count + 1
+    """ Runs YOUR specific part of the project """
 
 
 main()
--- src/m2.py
+++ src/m2.py
@@ -3,40 +3,12 @@
   Fall term, 2018-2019.
 """
 
-import low_level_rosebotics as rb
+import rosebotics as rb
 import time
 
 
 def main():
-    """ Runs tests. """
-    run_tests()
-
-
-def run_tests():
-    """ Runs various tests. """
-    run_test_go_stop()
-
-
-def run_test_go_stop():
-    """ Tests the   go   and   stop   Snatch3rRobot methods. """
-    robot = rb.Snatch3rRobot()
-
-    robot.go(50, 25)
-    time.sleep(2)
-    robot.stop()
-
-    print(robot.right_wheel.get_degrees_spun())
-    print(robot.left_wheel.get_degrees_spun())
-    robot.left_wheel.reset_degrees_spun(0)
-
-    time.sleep(2)
-
-    robot.go(100, 100)
-    time.sleep(3)
-    robot.stop(rb.StopAction.COAST.value)
-
-    print(robot.right_wheel.get_degrees_spun())
-    print(robot.left_wheel.get_degrees_spun())
+    """ Runs YOUR specific part of the project """
 
 
 main()
