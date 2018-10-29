f5a9be545003d356925f55b27590cc70b9bba8f2
zhuy9 <zhuy9@rose-hulman.edu>
2018-10-28 23:40:32 -0400

    advanced method of tracing the black line done to be tested
    it can figure out if it is going in clockwise or counter-

--- src/zhuy9.py
+++ src/zhuy9.py
@@ -25,11 +25,33 @@ def make_polygon(n):
 
 def follow_the_black_line():
     robot = rb.Snatch3rRobot()
-    robot.color_sensor.get_color()
+    color = robot.color_sensor.get_color()
+    print(color)
     while True:
         robot.drive_system.start_moving(100, 100)
         if robot.color_sensor.get_color() != 1:
             robot.drive_system.stop_moving()
             robot.drive_system.start_moving(-14, 99)
+
+def follow_black_line_advanced():
+    rem_direction = 0
+    robot = rb.Snatch3rRobot()
+
+    if robot.color_sensor.get_color() == 1:
+        robot.drive_system.go_straight_inches(2)
+        robot.drive_system.spin_in_place_degrees(-45)
+
+        if robot.color_sensor.get_color() == 1:
+            rem_direction = 1
+        else:
+            rem_direction = -1
+        robot.drive_system.stop_moving(45)
+
+        while True:
+            robot.drive_system.start_moving(100, 100)
+            if robot.color_sensor.get_color() != 1:
+                robot.drive_system.stop_moving()
+                robot.drive_system.start_moving(-14 * rem_direction, 99 * rem_direction)
+
 
 main()
