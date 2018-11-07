0195ab47e7b1ca2b9e4b545f90589e7a7e2188ae
wolfebn <wolfebn@rose-hulman.edu>
2018-11-07 11:22:55 -0500

    capstone 1 progress

--- src/capstone_1_runs_on_laptop.py
+++ src/capstone_1_runs_on_laptop.py
@@ -12,36 +12,36 @@ Authors:  David Mutchler, his colleagues, and Bryan Wolfe.
 # ------------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------------
-# TODO: 2. With your instructor, discuss the "big picture" of laptop-robot
-# TODO:    communication:
-# TODO:      - One program runs on your LAPTOP.  It displays a GUI.  When the
-# TODO:        user presses a button intended to make something happen on the
-# TODO:        ROBOT, the LAPTOP program sends a message to its MQTT client
-# TODO:        indicating what it wants the ROBOT to do, and the MQTT client
-# TODO:        SENDS that message TO a program running on the ROBOT.
-# TODO:
-# TODO:      - Another program runs on the ROBOT. It stays in a loop, responding
-# TODO:        to events on the ROBOT (like pressing buttons on the IR Beacon).
-# TODO:        It also, in the background, listens for messages TO the ROBOT
-# TODO:        FROM the program running on the LAPTOP.  When it hears such a
-# TODO:        message, it calls the method in the DELAGATE object's class
-# TODO:        that the message indicates, sending arguments per the message.
-# TODO:
-# TODO:  Once you understand the "big picture", delete this TODO (if you wish).
+# Done: 2. With your instructor, discuss the "big picture" of laptop-robot
+# Done:    communication:
+# Done:      - One program runs on your LAPTOP.  It displays a GUI.  When the
+# Done:        user presses a button intended to make something happen on the
+# Done:        ROBOT, the LAPTOP program sends a message to its MQTT client
+# Done:        indicating what it wants the ROBOT to do, and the MQTT client
+# Done:        SENDS that message TO a program running on the ROBOT.
+# Done:
+# Done:      - Another program runs on the ROBOT. It stays in a loop, responding
+# Done:        to events on the ROBOT (like pressing buttons on the IR Beacon).
+# Done:        It also, in the background, listens for messages TO the ROBOT
+# Done:        FROM the program running on the LAPTOP.  When it hears such a
+# Done:        message, it calls the method in the DELAGATE object's class
+# Done:        that the message indicates, sending arguments per the message.
+# Done:
+# Done:  Once you understand the "big picture", delete this Done (if you wish).
 # ------------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------------
-# TODO: 3. One team member: change the following in mqtt_remote_method_calls.py:
-#                LEGO_NUMBER = 99
-# TODO:    to use YOUR robot's number instead of 99.
-# TODO:    Commit and push the change, then other team members Update Project.
-# TODO:    Then delete this TODO.
+# Done: 3. One team member: change the following in mqtt_remote_method_calls.py:
+#                LEGO_NUMBER = 10
+# Done:    to use YOUR robot's number instead of 99.
+# Done:    Commit and push the change, then other team members Update Project.
+# Done:    Then delete this Done.
 # ------------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------------
-# TODO: 4. Run this module.
-# TODO:    Study its code until you understand how the GUI is set up.
-# TODO:    Then delete this TODO.
+# Done: 4. Run this module.
+# Done:    Study its code until you understand how the GUI is set up.
+# Done:    Then delete this Done.
 # ------------------------------------------------------------------------------
 
 import tkinter
@@ -52,17 +52,21 @@ import mqtt_remote_method_calls as com
 def main():
     """ Constructs and runs a GUI for this program. """
     root = tkinter.Tk()
-    setup_gui(root)
+
+    client = com.MqttClient()
+    client.connect_to_ev3()
+
+    setup_gui(root, client)
 
     root.mainloop()
     # --------------------------------------------------------------------------
-    # TODO: 5. Add code above that constructs a   com.MqttClient   that will
-    # TODO:    be used to send commands to the robot.  Connect it to this pc.
-    # TODO:    Test.  When OK, delete this TODO.
+    # Done: 5. Add code above that constructs a   com.MqttClient   that will
+    # Done:    be used to send commands to the robot.  Connect it to this ev3.
+    # Done:    Test.  When OK, delete this Done.
     # --------------------------------------------------------------------------
 
 
-def setup_gui(root_window):
+def setup_gui(root_window, client):
     """ Constructs and sets up widgets on the given window. """
     frame = ttk.Frame(root_window, padding=10)
     frame.grid()
@@ -74,13 +78,16 @@ def setup_gui(root_window):
     go_forward_button.grid()
 
     go_forward_button['command'] = \
-        lambda: handle_go_forward()
+        lambda: handle_go_forward(speed_entry_box, client)
 
 
-def handle_go_forward():
+def handle_go_forward(entry_box, client):
     """
     Tells the robot to go forward at the speed specified in the given entry box.
     """
+    speed_string = entry_box.get()
+    client.send_message('go_forward', [speed_string])
+    print('Sending the go_forward message with speed', speed_string)
     # --------------------------------------------------------------------------
     # TODO: 6. This function needs the entry box in which the user enters
     # TODO:    the speed at which the robot should move.  Make the 2 changes
--- src/capstone_1_runs_on_robot.py
+++ src/capstone_1_runs_on_robot.py
@@ -7,16 +7,16 @@ Also: responds to Beacon button-presses by beeping, speaking.
 This module runs on the ROBOT.
 It uses MQTT to RECEIVE information from a program running on the LAPTOP.
 
-Authors:  David Mutchler, his colleagues, and PUT_YOUR_NAME_HERE.
+Authors:  David Mutchler, his colleagues, and Bryan Wolfe.
 """
 # ------------------------------------------------------------------------------
-# TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.  Then delete this TODO.
+# Done: 1. PUT YOUR NAME IN THE ABOVE LINE.  Then delete this Done.
 # ------------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------------
-# TODO: 2. With your instructor, review the "big picture" of laptop-robot
-# TODO:    communication, per the comment in mqtt_sender.py.
-# TODO:    Once you understand the "big picture", delete this TODO.
+# Done: 2. With your instructor, review the "big picture" of laptop-robot
+# Done:    communication, per the comment in mqtt_sender.py.
+# Done:    Once you understand the "big picture", delete this Done.
 # ------------------------------------------------------------------------------
 
 import rosebotics_new as rb
@@ -27,15 +27,17 @@ import ev3dev.ev3 as ev3
 
 def main():
     # --------------------------------------------------------------------------
-    # TODO: 3. Construct a Snatch3rRobot.  Test.  When OK, delete this TODO.
+    # Done: 3. Construct a Snatch3rRobot.  Test.  When OK, delete this Done.
     # --------------------------------------------------------------------------
-
+    robot = rb.Snatch3rRobot()
     # --------------------------------------------------------------------------
-    # TODO: 4. Add code that constructs a   com.MqttClient   that will
-    # TODO:    be used to receive commands sent by the laptop.
-    # TODO:    Connect it to this robot.  Test.  When OK, delete this TODO.
+    # Done: 4. Add code that constructs a   com.MqttClient   that will
+    # Done:    be used to receive commands sent by the laptop.
+    # Done:    Connect it to this robot.  Test.  When OK, delete this Done.
     # --------------------------------------------------------------------------
-
+    rc = RemoteControlEtc(robot)
+    client = com.MqttClient(rc)
+    client.connect_to_pc()
     # --------------------------------------------------------------------------
     # TODO: 5. Add a class for your "delegate" object that will handle messages
     # TODO:    sent from the laptop.  Construct an instance of the class and
@@ -57,6 +59,20 @@ def main():
         # TODO:    Beacon is pressed.  Test.  When done, delete this TODO.
         # ----------------------------------------------------------------------
         time.sleep(0.01)  # For the delegate to do its work
+
+class RemoteControlEtc(object):
+    def __init__(self, robot):
+        self.robot = robot
+        """
+        Stores the robot.
+        :type robot: rb.Snatch3rRobot
+        """
+
+    def go_forward(self, speed_string):
+        """Makes the robot go forward at the given speed."""
+        print("Telling the robot to start moving at", speed_string)
+        speed = int(speed_string)
+        self.robot.drive_system.start_moving(speed, speed)
 
 
 main()
